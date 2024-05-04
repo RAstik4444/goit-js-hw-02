2ad41496e1577a74621b5de090c8873819417992
Rostyslav Ruchytsia <rastik.s96@gmail.com>
2024-05-04 18:17:04 +0200

    makeTransaction

--- makeTransaction.js
+++ makeTransaction.js
@@ -1,1 +1,8 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {}
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+        return "Insufficient funds!";
+    } else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
+}
